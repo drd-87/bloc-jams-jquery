c6143764d5b379151c5d09dcaac2e10950ffab89
David <drd.087@gmail.com>
2018-11-11 13:45:40 -0800

    Checkpoint 11 BlocJam jQuery Sliders: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -21,11 +21,26 @@
         const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
         let previousSongIndex = currentSongIndex - 1;
         if (previousSongIndex < 0) { return; }
-   
+
         const previousSong = album.songs[previousSongIndex];
         player.playPause(previousSong);
 
       });
+//change song's playback position
+$('#time-control').on('input', function(event) {
+  player.skipTo(event.target.value);
+});
+
+//update time control range input per second
+setInterval( () => {
+   if (player.playState !== 'playing') { return; }
+   const currentTime = player.getTime();
+   const duration = player.getDuration();
+   const percent = (currentTime / duration) * 100;
+   $('#time-control .current-time').text( currentTime );
+   $('#time-control input').val(percent);
+ }, 1000);
+
 
 //terminate block
- }
+}
